e94b4f873ae81ded402c1e846bba1c62c348bb94
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2022-07-18 23:33:22 +0200

    Fixed som Stuff in Settings

--- 80´s os/System/settings.py
+++ 80´s os/System/settings.py
@@ -1,53 +1,54 @@
 from pick import pick
 import webbrowser
 import os
+def pick_os():
+    title = f"ColdOS Settings"
+    options = ["Change Username","Change Password", "Change Email", "Info","Exit"]
+    selection = pick(options, title, indicator='=>', default_index=0)
+    assert len(selection) == 1
+    option, index = selection[0]
+    return option
 
-try:
-    with open("Strings/user_config.json", "r") as f:
-        settings = f.read()
-except:
-    with open("System/Strings/user_config.json", "r") as f:
-        settings = f.read()
-        
-title = f"Welcome {user} to ColdOS"
-options = ["Change Username","Change Password", "Change Email", "Info","Exit"]
-selection = pick(options, title, indicator='=>', default_index=0)
-assert len(selection) == 1
-option, index = selection[0]
-
+option = pick_os()
 if option == "Change Username":
     try:
         with open("Strings/user_config.json", "w") as f:
             name = input("Enter new username: ")
         config["Username"] = name
         json.dump(config,f)
+        pick_os()
     except:
         with open("System/Strings/user_config.json", "w") as f:
             name = input("Enter new username: ")
         config["Username"] = name
         json.dump(config,f)
+        pick_os()
 elif option == "Change Password":
     try:
         with open("Strings/user_config.json", "w") as f:
             name = input("Enter new Password: ")
         config["Password"] = name
         json.dump(config,f)
+        pick_os()
     except:
         with open("System/Strings/user_config.json", "w") as f:
             name = input("Enter new Password: ")
         config["Password"] = name
         json.dump(config,f)
+        pick_os()
 elif option == "Change Email":
     try:
         with open("Strings/user_config.json", "w") as f:
             name = input("Enter new Email: ")
         config["Email"] = name
         json.dump(config,f)
+        pick_os()
     except:
         with open("System/Strings/user_config.json", "w") as f:
             name = input("Enter new Email: ")
         config["Email"] = name
         json.dump(config,f)
+        pick_os()
 elif option == "Info":
     os.system("python System/debug.py")
 elif option == "Exit":
